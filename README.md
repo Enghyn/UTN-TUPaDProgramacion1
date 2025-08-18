2cce8f877e5791bb4dcf19d3f5168ebae4644562
Enzo Giaquinta <enzo.n.giaquinta@gmail.com>
2025-08-18 10:31:02 -0300

    readme agregado, ejercicios complementarios completado

---
+++ README.md
@@ -0,0 +1,4 @@
+# Programacion_1
+Tecnicatura en Programación UTN
+Estudiante: Enzo Giaquinta
+Comisión: 4
---
+++ ejercicios_complementarios.py
@@ -0,0 +1,64 @@
+#Estudiante: Enzo Giaquinta
+#Ejercicios complementarios
+
+#1
+numero1 = 18
+
+#2
+numero2 = 1.75
+
+#3
+suma = numero1 + numero2
+
+#4
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print(f"Resultado de la suma: {suma}")
+print(f"Resultado de la resta: {resta}")
+print(f"Resultado de la multiplicacion: {multiplicacion}")
+print(f"Resultado de la division: {division}")
+
+#5
+nombre = "Enzo"
+
+#6
+precio = 5000.16
+
+#7
+descuento = 0.15
+
+#8
+precio_final = precio - (precio * descuento)
+
+#9
+cadena = "Hola como estan?"
+
+#10
+longitud = len(cadena)
+
+#11
+precio = 18.45
+precio = int(precio)
+
+#12
+nombre = "Enzo"
+apellido = "Giaquinta"
+nombre_completo = nombre + " " + apellido
+
+#13
+edad = 18
+edad += 5
+edad -= 10
+
+#14
+altura = 1.75
+altura *= 4
+altura /= 3
+
+#15
+nombre_completo_mayusculas = "ENZO GIAQUINTA"
+nombre_completo_minusculas = nombre_completo_mayusculas.lower()
+
+#16
+nombre_completo = nombre_completo_mayusculas.capitalize()
