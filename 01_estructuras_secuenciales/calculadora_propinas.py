1fb273d781186c12bfc742a38ff412f828cbef84
Enzo Giaquinta <enzo.n.giaquinta@gmail.com>
2025-08-20 15:58:34 -0300

    ejercicio calculadora de propinas realizado

---
+++ 01_estructuras_secuenciales/calculadora_propinas.py
@@ -0,0 +1,7 @@
+monto_total = int(input("Ingrese el monto total de la cuenta: "))
+propina_sugerida = monto_total * 0.10
+print(f"Propina sugerida(10%): {propina_sugerida}")
+print(f"Total a pagar(10%): {monto_total + propina_sugerida}")
+propina_sugerida = monto_total * 0.15
+print(f"Propina sugerida(15%): {propina_sugerida}")
+print(f"Total a pagar(15%): {monto_total + propina_sugerida}")
