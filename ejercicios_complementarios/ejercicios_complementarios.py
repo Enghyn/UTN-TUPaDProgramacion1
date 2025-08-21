f4ca5db30e95c62ba93e1eb489162d2c8a4c3332
Enzo Giaquinta <enzo.n.giaquinta@gmail.com>
2025-08-21 08:41:30 -0300

    corrección ejercicios_complemetarios

--- ejercicios_complementarios/ejercicios_complementarios.py
+++ ejercicios_complementarios/ejercicios_complementarios.py
@@ -3,12 +3,15 @@
 
 #1
 numero1 = 18
+print(f"Numero 1: {numero1}\n")
 
 #2
 numero2 = 1.75
+print(f"Numero 2: {numero2}\n")
 
 #3
 suma = numero1 + numero2
+print(f"Suma: {suma}\n")
 
 #4
 resta = numero1 - numero2
@@ -21,44 +24,56 @@ print(f"Resultado de la division: {division}")
 
 #5
 nombre = "Enzo"
+print(f"Nombre: {nombre}\n")
 
 #6
 precio = 5000.16
+print(f"Precio: {precio}\n")
 
 #7
 descuento = 0.15
+print(f"Descuento: {descuento}\n")
 
 #8
 precio_final = precio - (precio * descuento)
+print(f"Precio final: {precio_final}\n")
 
 #9
 cadena = "Hola como estan?"
+print(f"Cadena: {cadena}\n")
 
 #10
 longitud = len(cadena)
+print(f"Longitud de la cadena: {longitud}\n")
 
 #11
 precio = 18.45
 precio = int(precio)
+print(f"Precio: {precio}\n")
 
 #12
 nombre = "Enzo"
 apellido = "Giaquinta"
 nombre_completo = nombre + " " + apellido
+print(f"Nombre completo: {nombre_completo}\n")
 
 #13
 edad = 18
 edad += 5
 edad -= 10
+print(f"Edad: {edad}\n")
 
 #14
 altura = 1.75
 altura *= 4
 altura /= 3
+print(f"Altura: {altura}\n")
 
 #15
 nombre_completo_mayusculas = "ENZO GIAQUINTA"
 nombre_completo_minusculas = nombre_completo_mayusculas.lower()
+print(f"Nombre completo en minúsculas: {nombre_completo_minusculas}\n")
 
 #16
 nombre_completo = nombre_completo_mayusculas.capitalize()
+print(f"Nombre completo con la primera letra en mayúsculas: {nombre_completo}")
