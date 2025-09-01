190c0687b73f4bcb4ac789b04a5b75e720bc1c12
Enzo Giaquinta <enzo.n.giaquinta@gmail.com>
2025-09-01 08:03:58 -0300

    error tipeo corregidon en tp_condicionales

--- 03_estructuras_condicionales/tp_estructuras_condicionales.py
+++ 03_estructuras_condicionales/tp_estructuras_condicionales.py
@@ -170,7 +170,7 @@ else:
 
 print("\n")
 
-# 7) En base a la tabla, escribir un programa que pregunte al usuario en cuál hemisferio se encuentra (N/S), qué mes
+# 10) En base a la tabla, escribir un programa que pregunte al usuario en cuál hemisferio se encuentra (N/S), qué mes
 # del año es y qué día es. El programa deberá utilizar esa información para imprimir por pantalla
 # si el usuario se encuentra en otoño, invierno, primavera o verano.
 
