2dee8d87b3a88561ce33559fa08a7bf76b69e184
Enzo Giaquinta <enzo.n.giaquinta@gmail.com>
2025-10-16 08:53:42 -0300

    terminado ejercicio archivos

--- Ejercicios_en_clase/apertura_archivos/ejercicios_complementarios_archivos.py
+++ Ejercicios_en_clase/apertura_archivos/ejercicios_complementarios_archivos.py
@@ -10,51 +10,54 @@ def esta_vacio():
 
 def iniciar_archivo():
     if esta_vacio():
-        with open(RUTA, "w", newline="") as archivo:
+        with open(RUTA, "w", newline="", encoding="UTF-8") as archivo:
             campos = ["nombre", "precio"]
-            escritor = csv.DictWriter(archivo, fieldnames=campos)
-            escritor.writeheader()
+            escritor = csv.writer(archivo)
+            escritor.writerow(campos)
 
 def mostrar_producto():
-    iniciar_archivo()
-    with open(RUTA, "r", newline="") as archivo:
+    with open(RUTA, "r", newline="", encoding="UTF-8") as archivo:
         lector = csv.reader(archivo)
         next(lector)
         for linea in lector:
             print(linea)
 
-def agregar_producto():
-    iniciar_archivo()
-    with open(RUTA, "a", newline="") as archivo:
-        while True:
-            try:
-                escritor = csv.writer(archivo)
-                nombre_producto = input("Ingrese el nombre del producto: ").strip()
-                precio_producto = float(input("Ingrese el precio del producto:  ").strip())
-                if precio_producto == 0:
-                    raise ValueError
-                escritor.writerow([nombre_producto, precio_producto])
-                break
-            except ValueError:
-                print("Ingrese un valor numérico")
-                continue
+def agregar_producto(nombre_producto:str, precio_producto:float):
+    with open(RUTA, "a", newline="", encoding="UTF-8") as archivo:
+        escritor = csv.writer(archivo)
+        escritor.writerow([nombre_producto, precio_producto])
+
 
 def buscar_producto(nombre_producto:str):
-    iniciar_archivo()
-    with open(RUTA, "r", newline="") as archivo:
-        lector = csv.DictReader(archivo)
+    with open(RUTA, "r", newline="", encoding="UTF-8") as archivo:
+        lector = csv.reader(archivo)
         for linea in lector:
-            if linea['nombre'] == nombre_producto:
-                return False
-            return [linea['nombre'], linea['precio']]
+            if linea[0] == nombre_producto:
+                return True
+        return False
 
 def eliminar_producto(nombre_producto:str):
-    iniciar_archivo()
-    with open(RUTA, "a", newline="") as archivo:
-        escritor = csv.DictWriter(archivo)
-        
-        escritor.writer
+    with open(RUTA, "r", newline="", encoding="UTF-8") as archivo:
+        lector = csv.reader(archivo)
+        lineas = [linea for linea in lector if linea[0] != nombre_producto]
+    
+    with open(RUTA, "w", newline="", encoding="UTF-8") as archivo:
+        escritor = csv.writer(archivo)
+        escritor.writerows(lineas)
 
+def actualizar_producto(nombre_producto:str, precio_nuevo:float):
+    with open(RUTA, "r", newline="", encoding="UTF-8") as archivo:
+        lector = csv.reader(archivo)
+        lineas = []
+        for linea in lector:
+            if linea[0] == nombre_producto:
+                linea[1] = precio_nuevo
+            lineas.append(linea)
+    with open(RUTA, "w", newline="", encoding="UTF-8") as archivo:
+        escritor = csv.writer(archivo)
+        escritor.writerows(lineas)
+
+iniciar_archivo()
 while True:
     try:
         print("""
@@ -73,19 +76,41 @@ while True:
             mostrar_producto()
 
         case 2:
-            agregar_producto()
+            try:
+                nombre_producto = input("Ingrese el nombre del producto: ").strip()
+                precio_producto = float(input("Ingrese el precio del producto:  ").strip())
+                if precio_producto == 0:
+                    raise ValueError
+                agregar_producto(nombre_producto, precio_producto)
+            
+            except ValueError:
+                print("Ingrese un valor numérico")
+        
         case 3:
-            producto = input("Ingrese el producto a buscar: ").strip()
-            if not buscar_producto(producto):
+            nombre_producto = input("Ingrese el producto a eliminar: ").strip()
+            if not buscar_producto(nombre_producto):
                 print("Producto no encontrado")
                 continue
-
+            eliminar_producto(nombre_producto)
 
         case 4:
-            pass
+            try:
+                nombre_producto = input("Ingrese el producto a actualizar: ").strip()
+                if not buscar_producto(nombre_producto):
+                    print("Producto no encontrado")
+                    continue
+                precio_producto = float(input("Ingrese el precio actualizado: ").strip())
+                if precio_producto == 0:
+                    raise ValueError
+                actualizar_producto(nombre_producto, precio_producto)
+            
+            except ValueError:
+                print("Ingrese un valor numérico")
+
         case 5:
             print("Saliendo...")
             break
+
         case _:
             print("Ingrese un número dentro del rango de opciónes")
 
